b1714f4ed628ee1371256d927eab5a69713749a2
Olivia Wong <olivia.wy.wong@gmail.com>
2021-09-13 00:55:13 +0000

    update to change counter to red when it turns negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
-  console.log(this);
   $("textarea").keyup(function () {
     $(this)
       .siblings()
       .children()
       .last()
       .text(140 - this.value.length);
+    const textLength = this.value.length;
+    const sum = 140 - textLength;
+    if (sum < 0) {
+      $(".counter").css({ color: "red" });
+    } else {
+      $(".counter").css({ color: "#545149" });
+    }
   });
 });
