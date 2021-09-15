7bd24339f0c5b7422e7d0245b62f466a31e1bb8b
Olivia Wong <olivia.wy.wong@gmail.com>
2021-09-15 05:55:32 +0000

    create renderTweets to render array and style tweet container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,21 +4,41 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
-  const tweetData = {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
+  const tweetData = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
     },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
     },
-    created_at: 1461116232227,
+  ];
+
+  const renderTweets = function (tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $("#tweet-container").append($tweet);
+    }
   };
 
   const createTweetElement = function (tweet) {
-    const $article = $("<article>");
     const userArticle = `
+    <article>
   <header>
     <div class="profile">
       <img src="${tweet.user.avatars}">
@@ -35,14 +55,10 @@ $(document).ready(function () {
     <i class="fa fa-heart"></i>
   </div>
   </footer>
+  </article>
 `;
-    $article.append(userArticle);
-    return $article;
+    return userArticle;
   };
 
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $("#tweet-container").append($tweet);
+  renderTweets(tweetData);
 });
